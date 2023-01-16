83ea36baadc218dae66ef4f0c7258ba38633c4be
gaayathrikk <101986018+gaayathrikk@users.noreply.github.com>
2023-01-16 23:19:30 +0530

    Update streamlit_app.py
    added header
    added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3')
+streamlit.text('Kale, spinach and oatmeal')
+streamlit.text('Boiled egg and milk')
